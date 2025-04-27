75f1eff4c4057629fe5b1da6f6dbc390272cac49
Dilip kumar <102877455+DiliipKumar@users.noreply.github.com>
2025-04-27 14:03:47 +0530

    Add files via upload

--- IAC/main.tf
+++ IAC/main.tf
@@ -1,1 +1,17 @@
-hi
+
+module "ec2_instance" {
+  source = "./modules/ec2-instance"
+
+  name               = "testing"
+  tags               = {
+    Environment = "Dev"
+    Owner       = "testing"
+  }
+  ami_id             = "ami-0e35ddab05955cf57"      
+  instance_type      = "t3.medium"                  
+  vpc_id             = "vpc-06af5d47e030e701a"       
+  subnet_id          = "subnet-0275ca1da3f21551d"    
+  security_group_ids = ["sg-05c1c05aea8884995"]     
+  region             = "ap-south-1"                 
+}
+    
