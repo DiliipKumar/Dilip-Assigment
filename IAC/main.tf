b13a57b2027aeae9bcc54fc92583aa111ca559d6
Dilip kumar <102877455+DiliipKumar@users.noreply.github.com>
2025-04-27 14:05:52 +0530

    Update main.tf

--- IAC/main.tf
+++ IAC/main.tf
@@ -10,8 +10,8 @@ module "ec2_instance" {
   ami_id             = "ami-0e35ddab05955cf57"      
   instance_type      = "t3.medium"                  
   vpc_id             = "vpc-06af5d47e030e701a"       
-  subnet_id          = "subnet-0275ca1da3f21551d"    
-  security_group_ids = ["sg-05c1c05aea8884995"]     
+  subnet_id          = "subnet-0435ca1da3f34562d"    
+  security_group_ids = ["sg-05c1c05aea8574378"]     
   region             = "ap-south-1"                 
 }
     
